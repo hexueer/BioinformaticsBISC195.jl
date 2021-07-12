853463fbc705589e8d2a73469fd4e4365427ba33
Ivy Ho <iho@wellesley.edu>
2021-07-11 23:56:21 -0400

    assignment09 completed

--- src/BioinformaticsBISC195.jl
+++ src/BioinformaticsBISC195.jl
@@ -15,8 +15,8 @@ export normalizeDNA,
     normalizeDNA(::AbstractString)
 
 Ensures that a sequence only contains valid bases
-(or `'N'` for unknown bases).
-Returns a LongDNASeq.
+(or `'N'` for unknown bases). All invalid bases are replaced with N's.
+Returns a the normalized DNA string
 """
 function normalizeDNA(seq)
     seq = uppercase(string(seq))
@@ -29,7 +29,7 @@ function normalizeDNA(seq)
 end
 
 """
-    basecomposition(sequence)
+    basecomposition(sequence::AbstractString)
 
 Counts the number of each type of base (A, C, G, T, N)
 in a DNA sequence and returns a dictionary of those counts
@@ -42,12 +42,12 @@ function composition(seq)
     return counts
 end
 
-# """
-#     gc_content(::LongDNASeq) or gc_content(seq::AbstractString)
+"""
+    gc_content(seq::AbstractString)
 
-# Calculates and returns the GC ratio of a DNA sequence.
-# The GC ratio is the total number of G and C bases divided by the total length of the sequence.
-# """
+Calculates and returns the GC ratio of a DNA sequence.
+The GC ratio is the total number of G and C bases divided by the total length of the sequence.
+"""
 # function gc_content(seq::LongDNASeq)
 #     seq = composition(seq)        
 #     return (seq[DNA_C] + seq[DNA_G]) / (seq[DNA_A] + seq[DNA_C] + seq[DNA_G] + seq[DNA_T] + seq[DNA_N])
@@ -58,6 +58,13 @@ function gc_content(seq::AbstractString)
     return (counts['C'] + counts['G']) / (counts['A'] + counts['C'] + counts['G'] + counts['T'] + counts['N'])
 end
 
+"""
+    complement(base::Char)
+    complement(seq::AbstractString)
+
+Takes a base and returns the complementary base.
+Also takes sequences and returns a sequence populated with the complementary bases.
+"""
 function complement(base::Char)
     comp = Dict('A'=>'T',
                 'T'=>'A',
@@ -71,6 +78,11 @@ function complement(seq::AbstractString)
     return map(complement, normalizeDNA(seq))
 end
 
+```
+    reverse_complement(seq::AbstractString)
+
+Takes a sequence and returns the reverse complement of that sequence.
+```
 function reverse_complement(seq::AbstractString)
     return reverse(complement(seq))
 end
@@ -98,6 +110,55 @@ function parse_fasta(path)
 	end
 	push!(sequences, join(tempSeq)) 
 	return headers, sequences
+end
+
+"""
+   kmercount(sequence, k)
+
+Finds and returns a vector of all kmers in a sequence
+
+Examples
+≡≡≡≡≡≡≡≡≡≡
+
+    julia> kmercount("ggg", 3)
+    Dict{Any,Any} with 1 entry:
+    "GGG" => 1
+
+    julia> kmercount("ATATATATA", 4)
+    Dict{Any,Any} with 2 entries:
+    "TATA" => 3
+    "ATAT" => 3
+
+    julia> kmercount("A", 2)
+    ERROR: k must be a positive integer less than the length of the sequence
+"""
+function  getKmers(sequence, k)
+    1 <= k <= length(sequence) || error("k must be a positive integer less than the length of the sequence")
+    kmers = []
+    
+    stopindex = length(sequence)-k+1
+
+    for i in 1:stopindex
+        kmer = normalizeDNA(sequence[i:i+k-1]) 
+        !occursin('N', kmer) && push!(kmers, kmer)
+    end
+
+    return Set(kmers)
+end
+
+"""
+    getKmerDist(kmer1, kmer2)
+
+Takes two kmer sets and calculates the distance between them.
+
+The distance will always be a positive number between 0 and 1 
+where identical things have a distance of 0 
+and things with absolutely no similarity have a distance of 1
+The distance metric is defined as 1 - (length of intersection / length of union), 
+or alternatively, as ((length of set1 - set2) + (length of set2 - set1)) / length of union.
+"""
+function getKmerDist(kmer1, kmer2)
+    return 1 - (length(intersect(kmer1, kmer2)) / length(union(kmer1, kmer2)))
 end
 
 end # module BioinformaticsBISC195
